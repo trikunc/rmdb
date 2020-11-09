704885e4f67b596cf03f3deb7b071fded454c457
kunc <kurookun@gmail.com>
2020-11-09 10:22:14 +0700

    first commit

---
+++ src/components/elements/LoadMoreBtn.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { StyledLoadMoreBtn } from "../styles/StyledLoadMoreBtn";
+
+const LoadMoreBtn = ({ text, callback }) => {
+  return (
+    <StyledLoadMoreBtn type="button" onClick={callback}>
+      {text}
+    </StyledLoadMoreBtn>
+  );
+};
+
+LoadMoreBtn.propTypes = {
+  text: PropTypes.string,
+  callback: PropTypes.func,
+};
+
+export default LoadMoreBtn;
---
+++ src/components/hooks/useMovieFetch.js
@@ -0,0 +1,51 @@
+import { useCallback, useEffect, useState } from "react";
+import { API_URL, API_KEY } from "../../config";
+
+export const useMovieFetch = (movieId) => {
+  const [state, setState] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const fetchData = useCallback(async () => {
+    setError(false);
+    setLoading(true);
+
+    try {
+      const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
+      const result = await (await fetch(endpoint)).json();
+      const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+      const creditsResult = await (await fetch(creditsEndpoint)).json();
+      const directors = creditsResult.crew.filter(
+        (member) => member.job === "Director"
+      );
+
+      setState({
+        ...result,
+        actors: creditsResult.cast,
+        directors,
+      });
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  }, [movieId]);
+
+  useEffect(() => {
+    if (localStorage[movieId]) {
+      console.log("Movie grab from local storage");
+      setState(JSON.parse(localStorage[movieId]));
+      setLoading(false);
+    } else {
+      console.log("Movie grab from API");
+      fetchData();
+    }
+  }, [fetchData, movieId]);
+
+  useEffect(() => {
+    localStorage.setItem(movieId, JSON.stringify(state));
+  }, [movieId, state]);
+
+  return [state, loading, error];
+};
+
+// export default useMovieFetch;
